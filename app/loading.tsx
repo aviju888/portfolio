09108b47f7bfd4863cf99304b2228128a98ba96f
Adriel V <avijuan@berkeley.edu>
2025-10-22 10:43:16 -0700

    clearing dir

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-[60vh]">
-      <div className="w-12 h-12 border-t-4 border-b-4 border-blue-500 rounded-full animate-spin"></div>
-    </div>
-  );
-} 
